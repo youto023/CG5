e60ebfc248f56ea7914024352b2a30ba89703b92
youto023 <yuuto9036@gmail.com>
2025-04-16 12:51:49 +0900

    p32まで完成

---
+++ DirectXGame/TestVS.hlsl
@@ -0,0 +1,14 @@
+struct VertexShaderOutput
+{
+    float4 position : SV_POSITION;
+};
+struct VertexShaderInput
+{
+    float4 position : POSITION0;
+};
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    output.position = input.position;
+    return output;
+}
