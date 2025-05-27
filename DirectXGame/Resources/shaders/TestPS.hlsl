0c7ebbc10c2cb24502a8fab265037737ba5e2c66
youto023 <yuuto9036@gmail.com>
2025-05-27 10:22:52 +0900

    エラー改ざん

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,10 +1,8 @@
-struct PixelShaderOutput
-{
-    float4 color : SV_TARGET0;
+struct PixelShaderOutput{
+    float4 color : SV_Target0;
 };
 
-PixelShaderOutput main()
-{
+PixelShaderOutput main(){
     PixelShaderOutput output;
     output.color = float4(1.0f, 1.0f, 1.0f, 1.0f);
     return output;
--- DirectXGame/Resources/shaders/TestVS.hlsl
+++ DirectXGame/Resources/shaders/TestVS.hlsl
@@ -1,15 +1,11 @@
-struct VertexShaderOutput
-{
+struct VertexShaderOutput{
     float4 position : SV_POSITION;
 };
-
-struct VertexShaderInput
-{
+struct VertexShaderInput{
     float4 position : POSITION0;
 };
 
-VertexShaderOutput main(VertexShaderInput input)
-{
+VertexShaderOutput main(VertexShaderInput input){
     VertexShaderOutput output;
     output.position = input.position;
     return output;
